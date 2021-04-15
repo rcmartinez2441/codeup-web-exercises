1aaa5e45a291fc6b266c21a9b636671f4c3e15e8
Raul Martinez <raulmartinez@rauls-mbp.lan>
2021-04-15 14:38:19 -0600

    starting to get the hang of pushing files to github

---
+++ js/operator-exercises.js
@@ -0,0 +1,76 @@
+'use strict';
+//Exercises
+//For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in the Chrome console.
+
+//'1' + 2
+// EXPECTED OUTPUT = 12
+console.log('1' + 2);
+
+//typeof '1' + 2
+//EXPECTED OUTPUT = string2
+console.log(typeof '1' + 2);
+
+//6 % 4
+//EXPECTED OUTPUT = 2
+console.log(6 % 4);
+
+//'3 + 4 is ' + 3 + 4
+// how could you make this produce the correct output?
+//EXPECTED OUTPUT = 3 + 4 is 34
+console.log('3 + 4 is ' + 3 + 4);
+
+//0 < 0
+//EXPECTED OUTPUT = false
+console.log(0 < 0);
+
+//'false' == false
+//EXPECTED OUTPUT = false
+console.log('false' == false);
+
+//true == 'true'
+//EXPECTED OUTPUT = false
+console.log(true == 'true');
+
+//5 >= -5
+//EXPECTED OUTPUT = true
+console.log(5 >= -5);
+
+//!false || false
+//EXPECTED OUTPUT = true
+console.log(!false || false);
+
+//true || "42"
+//EXPECTED OUTPUT = true
+console.log(true || "42");
+
+//!true && !false
+//EXPECTED OUTPUT = false
+console.log(!true && !false);
+
+//6 % 5
+//EXPECTED OUTPUT = 1
+console.log(6 % 5);
+
+//5 < 4 && 1 === 1
+//EXPECTED OUTPUT = false
+console.log(5 < 4 && 1 === 1);
+
+//typeof 'codeup' === 'string'
+//EXPECTED OUTPUT = true
+console.log(typeof 'codeup' === 'string');
+
+//'codeup' === 'codeup' && 'codeup' === 'Codeup'
+//EXPECTED OUTPUT = false
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+
+//4 >= 0 && 1 !== '1'
+//EXPECTED OUTPUT = true
+console.log(4 >= 0 && 1 !== '1');
+
+//6 % 3 === 0
+//EXPECTED OUTPUT = true
+console.log(6 % 3 === 0);
+
+//5 % 2 !== 0
+//EXPECTED OUTPUT = true
+console.log(5 % 2 !== 0);
--- js/primitive-types.js
+++ js/primitive-types.js
@@ -16,4 +16,19 @@ false - boolean
 true - boolean
 
 'true' - string
+
+What data type would best represent:
+
+A term or phrase typed into a search box?
+- string
+If a user is logged in?
+- boolean
+A discount amount to apply to a user's shopping cart?
+-number
+Whether or not a coupon code is valid?
+-boolean
+An email address typed into a registration form?
+- string
+The price of a product?
+- number
  */
