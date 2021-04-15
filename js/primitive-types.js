00a8e561de0d3cb7032cdba100a5155be858a2e0
Raul Martinez <raulmartinez@rauls-mbp.lan>
2021-04-15 14:17:13 -0600

    pushing is fun

---
+++ js/primitive-types.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+Identify the data type of the following values
+
+99.9 - Number
+
+"false" - String
+
+false - boolean
+
+'0' - string
+
+0 - number
+
+true - boolean
+
+'true' - string
+ */
