50a184497d5972e5a400ce4dbf3c2fa91315a83d
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-04-22 16:10:32 -0600

    updating loops exercises and lecture notes

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -13,37 +13,103 @@
 
 // TODO: MINI EXERCISES
 // TODO: print all even numbers between 0 and 25;
-function coutingEvenNumbers () {
-	let evenNumbers = 1;
-	while (evenNumbers <= 25) {
-		if (evenNumbers % 2 === 0) {
-			console.log(`current iteration is: ${evenNumbers}`);
-		}
-		evenNumbers += 1;
-		console.log(`------------------------------------------------`);
-	}
-}
-coutingEvenNumbers();
+// function coutingEvenNumbers () {
+// 	let evenNumbers = 1;
+// 	while (evenNumbers <= 25) {
+// 		if (evenNumbers % 2 === 0) {
+// 			console.log(`current iteration is: ${evenNumbers}`);
+// 		}
+// 		evenNumbers += 1;
+// 		console.log(`------------------------------------------------`);
+// 	}
+// }
+// coutingEvenNumbers();
+
 // TODO: using prompt() and alert(), write a loop that will continue to ask for the correct password until the
-function isPassword() {
-	while (true) {
-		let passwordInput = prompt('Please Enter Password');
-		let savedPassword = 'bonelesspizza';
-		if (passwordInput === savedPassword) { //if user input equals 'bonelesspizza' it will alert and exit loop
-			alert('This is the password');
-			break;
-		} else if (passwordInput === null) { //If user presses cancel, it will exit loop too
-			break;
-		} else { //If above two fail, alert will execute and loop will continue otherwise
-			alert('This not the password, try again');
-		}
-	}
-}
-isPassword();
+// function isPassword() {
+// 	while (true) {
+// 		let passwordInput = prompt('Please Enter Password');
+// 		let savedPassword = 'bonelesspizza';
+// 		if (passwordInput === savedPassword) { //if user input equals 'bonelesspizza' it will alert and exit loop
+// 			alert('This is the password');
+// 			break;
+// 		} else if (passwordInput === null) { //If user presses cancel, it will exit loop too
+// 			break;
+// 		} else { //If above two fail, alert will execute and loop will continue otherwise
+// 			alert('This not the password, try again');
+// 		}
+// 	}
+// }
+// isPassword();
 // correct password is given.
 //hint: start by defining the correct password and don't forget to link your html file
 
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
 
 // //Do-While Loops
+// Will always execute the code at least once before the condition is checked
+//SYNTAX
+/*
+	do {
+		[code to execute];
+	} while( condition );
+ */
+
+// print 'we have 'x' amount of desks left'
+// 1. variable that holds the amount of student --. starting point
+// 2. Define amount of desks avail --> place as condition inside the while ( )
+// 2. start the 'do' loop
+// 3.
+
+// let numOfStudents = 0;
+// do {
+// 	var desksAvailable = 20 - numOfStudents;
+// 	console.log(`We have ${desksAvailable} amount of desks left`);
+// 	numOfStudents++;
 //
+// } while( numOfStudents <= 20);
+//
+// console.log('Sorry Class is Full!')
+
+
+//Sing happy bday to someone every year on their birthday and when they reach 16 say, 'I got you a car'
+//variable = age starting point
+
+// let age = 0;
+// do {
+// 	age++
+// 	console.log(`Happy Birthday to you! you are now ${age} years old!`)
+// } while (age < 15);
+//
+// console.log('too old for b-day here is a car');
+// to old now, here's a job application and car to
+//
+
+// TODO EXERCISE
+// // TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+//  Let's assume that there are allowing two cars into the parking lot at a time. As those cars take up spaces,
+//  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+//  is now full" to the console.
+// // HINT: think of a countdown, remember if statements can be used inside of your statements
+
+// let numberOfParking = () => {
+// 	let totalParkingSpace = 30;
+// 	let carsParked = 0;
+// 	do {
+// 		console.log(`There are currently ${totalParkingSpace - carsParked} parking spaces available`)
+// 		carsParked += 2;
+// 		console.log(`Some cars are about to park`)
+// 		console.log(`-------------------------------------------`)
+// 	} while (totalParkingSpace >= 0);
+// 	console.log(`There are no more spaces available. Total cars parked are: ${carsParked}`);
+// }
+// numberOfParking();
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+
 // //For Loops
---
+++ js/while.js
@@ -0,0 +1,77 @@
+(function () {
+/*
+Create a file named while.js in the js directory.
+
+Create a while loop that uses console.log() to create the output shown below:
+2
+4
+8
+16
+32
+64
+128
+256
+512
+1024
+2048
+4096
+8192
+16384
+32768
+65536
+ */
+
+	'use strict';
+	let whileLoop = () => {
+		let number = 2;
+		let counter = 1;
+		while(counter <=
+		16) {
+			console.log(number);
+			number*=2;
+			counter++;
+		}
+	}
+	whileLoop();
+
+/*
+An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random
+number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your
+code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients.
+Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers
+for this exercise.
+ */
+// The output should be similar to the following:
+//
+// 	5 cones sold...  // if there are enough cones
+// Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+// Yay! I sold them all! // If there are no more cones
+let sellingUntilOut = () => {
+	// This is how you get a random number between 50 and 100
+	var allCones = Math.floor(Math.random() * 50) + 50;
+	// This expression will generate a random number between 1 and 5
+	let conesSold = 0;
+	do {
+		console.log(`Total cones remaining for the day is ${allCones}`);
+		conesSold = Math.floor(Math.random() * 5) + 1;
+		if (conesSold <= allCones) {
+			console.log(`We just sold ${conesSold} cones!`)
+			allCones -= conesSold;
+		} else {
+			console.log(`Sorry, I cant sell you ${conesSold} cones, I only have ${allCones}`);
+			console.log('We sold them the remainder of the cones we had');
+			allCones -= allCones;
+		}
+	} while (allCones !== 0);
+	console.log('All out of cones! time to go home');
+}
+sellingUntilOut();
+
+
+
+
+
+
+
+
+})();
