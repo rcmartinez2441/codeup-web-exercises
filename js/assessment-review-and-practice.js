ce85c841433d05a244222de540beb21a0621abe8
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-04-29 08:10:35 -0600

    added to curriculum notes

---
+++ js/assessment-review-and-practice.js
@@ -0,0 +1,28 @@
+(function (){
+	'use strict';
+//***  04/28/21 - Assessment Review ***
+// They are currently reviewing
+
+//*** 04/28/21 ***
+//2nd Assessment - Post Test walkthrough with Tristan
+
+//Triatan created a helper function before hand called "numbify" that would
+ // Number.isNaN(value) will work like a === where it will check explicitly check if
+//	Number.isNaN();
+
+//For getLowestNumber
+// Tristan used Math.min( value1, value2, ... )
+// Will return
+
+// function replace( str, find, replace) {
+// 		if (typeof str === 'string'){
+// 			return str.replace(find, replace)
+// 		}
+// }
+
+
+
+
+
+
+})();
