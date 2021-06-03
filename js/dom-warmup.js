656be799feef2dc1e023550baa192e392a81b8ed
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-06-03 12:33:01 -0600

    working on more jquery event exercises

--- js/dom-warmup.js
+++ js/dom-warmup.js
@@ -10,12 +10,16 @@
 				.addClass('container row d-flex justify-content-center m-auto')
 				.append(
 					`<div id="for-text" class="col-12 text-center"></div> <br>
-					<img src="assets/dj-khaled.gif" alt="dj-khaled" class="col-12" height="200px" width="50px"/>`);
+					<img src="assets/dj-khaled.gif" alt="dj-khaled" class="col-12" height="300px"/>`);
 			$('#for-text')
 				.text(inputValue)
 				.css({
+					'border': 'solid 2px black',
+					'border-radius': '5px',
 					'font-size': '40px',
-					'font-weight': 'bold'
+					'font-weight': 'bold',
+					'color': 'white',
+					'background':'#ff449f'
 				})
 
 
