26516d04774e9bc05c82f2ed70d26c8d4e0a53ae
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-06-09 17:50:16 -0600

    working on mapbox and past assignments

---
+++ js/mapbox-playground.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const mapboxToken = 'pk.eyJ1IjoicmNtYXJ0aW5lejI0NDEiLCJhIjoiY2twcG44NmFiMm9xNzMycGVjbXVpbWVxYSJ9.Iqz3jC7dpe5lFyuA6iVjdg'
+
+mapboxgl.accessToken = mapboxToken;
+let map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/dark-v10',
+	center: [135.7460400, 35.4957600], //Obama, Japan
+	zoom: 10
+})
+
+let marker = setMarker([135.7460400, 35.4957600])
+
+addMapClickEvent(marker);
+
+let geoCoder = setGeocoder();
+addGeocoderToMap(geoCoder);
+addGeocoderEvent(geoCoder);
+setPopup('Obama')
+
+function setGeocoder() {
+	return new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl,
+		marker: false // dont use default marker, use mine
+	})
+}
+
+function addGeocoderToMap(geocoder) {
+	map.addControl(geocoder);
+}
+
+//Adds even listener to geocoder when you select an option when you search
+function addGeocoderEvent(geocoder) {
+	geocoder.on('result', function (event) {
+		marker.setLngLat(event.result.geometry.coordinates);
+		console.log(event.result);
+		setPopup(event.result.place_name);
+	})
+}
+
+function setMarker(point) {
+	return new mapboxgl
+		.Marker()
+		.setLngLat(point)
+		.addTo(map);
+}
+
+function addMapClickEvent(markerParam) {
+	map.on('click', function (e) {
+		markerParam.setLngLat(e.lngLat)
+			.addTo(map);
+	})
+}
+
+
+//Creating PopUps
+function setPopup(textDetails) {
+	let popup = new mapboxgl.Popup()
+		.setHTML(`<p>${textDetails}</p>`)
+		.addTo(map)
+	marker.setPopup(popup); //whenever selPopup is called, it will add the popup to the marker
+}
+
