ed7a4bdbec75b91b3e21cd10022f61ef4d67ad48
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-06-15 19:20:45 -0600

    testing current local systems time using git log

--- js/promises.js
+++ js/promises.js
@@ -1,8 +1,20 @@
-fetch('https://api.github.com/users/rcmartinez2441/events/public')
-	.then(response => {
-		console.log(response.json())
+function fetchLastCommit() {
+	let username = $('#github-username').val()
+	console.log(username);
+	fetch(`https://api.github.com/users/${username}/events/public`, {
+		headers: {
+			'Authorization': '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'
+		}
 	})
-	.catch(error => {
-		console.log(error)
-		console.log(`you idiot`)
-	})
+		.then(response => {
+			console.log(response.json())
+		})
+		.catch(error => {
+			console.log(error)
+			console.log(`you idiot`)
+		})
+}
+
+let submitButton = $('#submit-button')
+
+submitButton.click(fetchLastCommit)
